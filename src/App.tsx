c440435e22489bfac48592729adb4e8800f5d69b
Tomasz Poniatowski <tomasz.poniatowski@cytawa.com>
2022-11-26 18:14:04 +0100

    -first page-

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {StartPage} from "./components/StartPage" ;
+import {Cart} from "./components/Cart" ;
+import {Resort} from "./components/Resort" ;
+import {ChakraProvider} from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ChakraProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<StartPage/>}></Route>
+                    <Route path="/resort" element={<Resort/>}></Route>
+                    <Route path="/cart" element={<Cart/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
+    );
 }
 
 export default App;
---
+++ src/components/Wraper.tsx
@@ -0,0 +1,16 @@
+import {Box, Stack} from "@chakra-ui/react";
+
+interface WrapperProps {
+    heading?: string;
+    children?: React.ReactNode;
+}
+
+export const Wrapper = (props: WrapperProps) => {
+    return (
+        <Box
+           // backgroundImage="url('/components/background.jpg')"
+            //backgroundPosition="center"
+            //backgroundRepeat="no-repeat"
+        />
+    );
+};
