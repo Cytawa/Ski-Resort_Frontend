4528df3dc26e68d5f07a902a2d44d52a7367c591
Tomasz Poniatowski <tomasz.poniatowski@cytawa.com>
2022-11-29 01:56:03 +0100

    -add page-

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,62 @@
-import React from 'react';
+import React, {createContext, useState} from 'react';
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {StartPage} from "./components/StartPage" ;
-import {Cart} from "./components/Cart" ;
-import {Resort} from "./components/Resort" ;
+import {StartPage} from "./components/StartPage";
+import {Cart} from "./components/Cart";
+import {Resort} from "./components/Resort";
 import {ChakraProvider} from "@chakra-ui/react";
+import {ResortData} from "./models/resorts"
+import {UserData} from "./models/user";
+import {RoleEnum} from "./models/roleEnum";
+
+interface DataContext {
+    resortData: ResortData;
+    resortDataModifier: (value: ResortData) => void;
+    userData: UserData;
+    userDataModifier: (value: UserData) => void;
+}
+
+export const DataContext = createContext<DataContext>({
+    resortData: {
+        curortAdress: "", curortName: "", curortPhonenumber: 0, currortEmail: "", id: 0,
+    },
+    userDataModifier: (value: UserData)=>{},
+    resortDataModifier: (value: ResortData) => {
+    },
+    userData:{userId:0,userName:"",userRole:RoleEnum.unsign}
+
+});
 
 function App() {
+    const [resortData, setResortData] = useState<ResortData>({
+
+        curortAdress: "",
+        curortName: "",
+        curortPhonenumber: 0,
+        currortEmail: "",
+        id: 0,
+
+    },);
+    const resortDataModifier = (value: ResortData) => {
+        setResortData(value);
+    };
+    const [userData, setUserData]=useState<UserData>({userId:0,userName:"Tomek",userRole:RoleEnum.unsign},);
+const userDataModifier=(value: UserData)=>{
+    setUserData(value)
+    }
     return (
-        <ChakraProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<StartPage/>}></Route>
-                    <Route path="/resort" element={<Resort/>}></Route>
-                    <Route path="/cart" element={<Cart/>}></Route>
-                </Routes>
-            </BrowserRouter>
-        </ChakraProvider>
+        <DataContext.Provider value={{resortData: resortData,
+            resortDataModifier: resortDataModifier, userData:userData,userDataModifier:userDataModifier}}>
+            <ChakraProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<StartPage/>}></Route>
+                        <Route path="/resort" element={<Resort/>}></Route>
+                        <Route path="/cart" element={<Cart/>}></Route>
+                    </Routes>
+                </BrowserRouter>
+            </ChakraProvider>
+        </DataContext.Provider>
     );
 }
 
---
+++ src/models/resorts.ts
@@ -0,0 +1,7 @@
+export interface ResortData {
+    id:number,
+    curortName: string,
+    curortAdress: string,
+    currortEmail:string,
+    curortPhonenumber:number,
+}
